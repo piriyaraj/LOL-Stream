d0e137fe4bc2abc136805208aff31bdef8d0ea33
piriyaraj <piriyaraj1998@gmail.com>
2023-08-04 17:56:29 +0530

    updated

--- video_maker/test.py
+++ video_maker/test.py
@@ -18,11 +18,10 @@ def run_in_new_terminal(command):
     # subprocess.Popen(['start', 'cmd', '/c', batch_script_path], shell=True)
 
 playerLink = "https://www.op.gg/summoners/kr/woxiangdazhiye/ingame"
-playrName = "woxiangdazhiye"
-
+playerName = playerLink.split("/")[-2]
 try:
-    playerTeam, playerIndex = get_commands(playerLink,playrName)
-   
+    playerTeam, playerIndex = get_commands(playerLink,playerName)
+
     print("Player Team:",playerTeam)
     print("Player Index:",playerIndex)
     # command = """ cd /d "C:\Riot Games\League of Legends\Game" & "League of Legends.exe" "spectator spectator-consumer.euw1.lol.pvp.net:80 YQHBRhh4BwsDqaJyYm6ulqnT1NkkKki8 6534915822 EUW1" "-UseRads" """
