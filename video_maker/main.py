2182f20f33bde1bb575a2550f68375af516f2b00
piriyaraj <piriyaraj1998@gmail.com>
2023-08-05 14:18:02 +0530

    updated

--- video_maker/main.py
+++ video_maker/main.py
@@ -7,22 +7,14 @@ from usecases.game_controller import ControlGamePlay
 
 if not os.path.exists("media/gameplay"):
     os.makedirs("media/gameplay")
-    
-def run_in_new_terminal(command):
-    cmds = command.split("&")
-    batch_script_path = os.path.abspath("media/gameplay/Runner.cmd")
-    
-    with open(batch_script_path, 'w') as file:
-        for cmd in cmds:
-            file.write(cmd.strip() + "\n")
-    # subprocess.Popen(['start', 'cmd', '/c', batch_script_path], shell=True)
 
-playerLink = input("Enter ingame player Link (EX:'https://www.op.gg/summoners/kr/참새크면비둘기/ingame'): ")
+
+# playerLink = input("Enter ingame player Link (EX:'https://www.op.gg/summoners/kr/참새크면비둘기/ingame'): ")
 # playerLink = "https://www.op.gg/summoners/kr/참새크면비둘기/ingame"
-playerName = playerLink.split("/")[-2]
+# playerName = playerLink.split("/")[-2]
 try:
-    playerTeam, playerIndex = get_commands(playerLink,playerName)
-
+    # playerTeam, playerIndex = get_commands(playerLink,playerName)
+    playerTeam, playerIndex ="Blue",3
     gameController = ControlGamePlay(playerTeam,playerIndex)
     video_file_name = gameController.control()
 except Exception as e:
