a3732024a892efe18e59b11cddafd904a873e4ab
piriyaraj <piriyaraj1998@gmail.com>
2023-08-08 00:02:59 +0530

    updated

--- video_maker/usecases/get_run_command.py
+++ video_maker/usecases/get_run_command.py
@@ -53,13 +53,14 @@ def scrape_lolpros_player(playerLink,playrName,driver,team,index,no_of_played_ga
         driver.get(player_url)
         while True:
             try:
-                is_gameplay_found = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='spectate css-1wruk4q eh5kfb0' and @type='button']")))
                 try :
                     button =WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@mode='primary' and @size='large' and contains(span, 'GODKÄNN')]")))
                     button.click()
                     time.sleep(1)
                 except Exception as e :
-                    print("Error(click cookies accept):",e)
+                    print("  - Cookies option not available")
+                is_gameplay_found = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='spectate css-1wruk4q eh5kfb0' and @type='button']")))
+
                 if team is not None :
                     updated_no_played_game = get_no_played_game(team,index,driver)
                     if no_of_played_game == updated_no_played_game:
