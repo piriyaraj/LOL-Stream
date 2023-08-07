7f88ae4facd85b88e3248ec3ac1824f4fee4bbcb
piriyaraj <piriyaraj1998@gmail.com>
2023-08-08 00:09:42 +0530

    updated

--- video_maker/entities/data_scrapper.py
+++ video_maker/entities/data_scrapper.py
@@ -11,7 +11,7 @@ class DataScrapper:
         self.__download_path = os.path.abspath(r'.\media\gameplay')
         # SELENIUM
         self.__options = Options()
-        self.__options.headless = True
+        # self.__options.headless = True
         self.__options.set_preference(
             'browser.download.dir', self.__download_path)
         self.__options.set_preference("network.cookie.cookieBehavior", 0)
